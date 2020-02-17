456302b3628a89986edd4031ed1f01e0b7a67c91
ShahriarSaleque <salequeshahriar@gmail.com>
2020-02-17 23:40:12 +0600

    factorial done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,32 @@ import React, { useState } from "react";
 
 export default function App() {
   const [number, setnumber] = useState(0);
+  const [factorial, setfactorial] = useState(0);
 
-  const factorial = () => {};
+  const submit = e => {
+    let i = 1;
+    let p = 1;
+    while (i <= number) {
+      p = p * i;
+      i = i + 1;
+    }
+    setfactorial(p);
+    e.preventDefault();
+  };
   return (
     <div>
       <h1>Factorial Calculator</h1>
-      <form onSubmit={this.factorial}>
-        <input type="number" name="number" placeholder="Enter a number..." />
+      <form>
+        <input
+          type="number"
+          name="number"
+          placeholder="Enter a number..."
+          onChange={e => setnumber(e.target.value)}
+        />
         <br />
-        <button>Calculate Factorial</button>
+        <button onClick={submit}>Calculate Factorial</button>
       </form>
-      <h2>Factorial: {0}</h2>
+      <h2>Factorial: {factorial}</h2>
     </div>
   );
 }
